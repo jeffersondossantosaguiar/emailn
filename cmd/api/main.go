af8024fc426638742107df16b83f55882ee3448d
Jefferson Aguiar <jefferson.aguiar@facens.br>
2024-07-22 11:37:36 -0300

    chore: add repository and air lib

---
+++ cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"emailn/internal/contract"
+	"emailn/internal/domain/campaign"
+	"emailn/internal/infrastructure/database"
+	internalerrors "emailn/internal/internal-errors"
+	"errors"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	service := campaign.Service{
+		Repository: &database.CampaignRepository{},
+	}
+
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaign
+		render.DecodeJSON(r.Body, &request)
+		id, err := service.Create(request)
+
+		if err != nil {
+			if errors.Is(err, internalerrors.ErrInternal) {
+				render.Status(r, http.StatusInternalServerError)
+			} else {
+				render.Status(r, http.StatusBadRequest)
+			}
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
+
+		render.Status(r, 201)
+		render.JSON(w, r, map[string]string{"id": id})
+	})
+
+	http.ListenAndServe(":3000", r)
+}
