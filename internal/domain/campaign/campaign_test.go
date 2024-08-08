aa22179184db67f6565c272490e7fe90e1b51901
Jefferson Aguiar <jefferson.aguiar@facens.br>
2024-08-08 19:26:49 -0300

    test: add services tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -32,6 +32,13 @@ func Test_NewCampaign_IDIsNotNill(t *testing.T) {
 	assert.NotNil(campaign.ID)
 }
 
+func Test_NewCampaign_MustStatusStartWithPending(t *testing.T) {
+	assert := assert.New(t)
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Equal(Pending, campaign.Status)
+}
+
 func Test_NewCampaign_CreateOnMustBeNow(t *testing.T) {
 	assert := assert.New(t)
 	now := time.Now().Add(-time.Minute)
